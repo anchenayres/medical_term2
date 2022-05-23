fb41d31606b54b55585b5cc7a245e8f440fbd094
anchenayres <21100284@virtualwindow.co.za>
2022-05-23 23:15:46 +0200

    logo
    Tried adding logo but wasn't happy and took it out.

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -10,12 +10,15 @@ const Navigation = () => {
         <div className="pictures">
         <h1>Welcome Back Dr.Manley</h1>
         <div className="pro-image"></div>
+
+    
   
        
     </div>
 
         <div className="nav"></div>
         <div className="side-nav">
+
                 <div className="planner-img"></div>
                 <div className="patients-img"></div>
                 <div className="docter-img"></div>
