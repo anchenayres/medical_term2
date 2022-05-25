628822567f5a89cbbb5eff49c470b12bf577b0ac
anchenayres <21100284@virtualwindow.co.za>
2022-05-25 09:17:40 +0200

    navigation
    added navigation to each page to start my sign up and sign in pages

--- src/Components/Appointments.js
+++ src/Components/Appointments.js
@@ -7,6 +7,44 @@ const Appointments = () => {
     return (
     <>
 
+<div className="pictures">
+        <h1>Welcome Back Dr.Manley</h1>
+        <div className="pro-image"></div>
+        </div>
+        <div className="search-bar">
+            <h15>Search</h15>
+        </div>
+        <ul className="login">
+            <li><a href="/Login">Sign In</a></li>
+        </ul>
+        <ul className="register">
+            <li><a href="/Register">Sign Up</a></li>
+        </ul>
+
+        <div className="nav"></div>
+        <div className="side-nav">
+        <div className="logo"></div>
+
+
+                <div className="planner-img"></div>
+                <div className="patients-img"></div>
+                <div className="docter-img"></div>
+                <div className="reception-img"></div>
+        <ul>
+            <li><a href="/Appointments">Appointments</a></li>
+            <li><a href="/Patients">Patients</a></li>
+            <li><a href="/Doctors">Doctors</a></li>
+            <li><a href="/Reception">Reception</a></li>
+        </ul>
+        </div>
+    
+
+    <div className="footer">
+        <div className="facebook"></div>
+        <div className="twitter"></div>
+        <div className="instagram"></div>
+    </div>
+    
     <div className="today-appointments">
         <div className="heading">
             <h12>Today's Appointments</h12>
