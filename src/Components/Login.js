0761822a7a134ad238d7301f820e8ab0b3ffa522
anchenayres <21100284@virtualwindow.co.za>
2022-05-25 10:43:18 +0200

    Sign up page
    sign up page styling complete

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -5,7 +5,23 @@ const Login = () => {
 
     return (
     <>
-    </>
+    <div className="behind">
+        <ul>
+            <li><a href="/Appointments">Appointments</a></li>
+            <li><a href="/Patients">Patients</a></li>
+            <li><a href="/Doctors">Doctors</a></li>
+            <li><a href="/Reception">Reception</a></li>
+        </ul>
+        
+   
+    </div>
+    <div className="login-heading">
+    <p1>Sign In</p1>
+        <div className="box1"><div className="username-login">Username</div></div>
+        <div className="box2"><div className="username-login">Password</div></div>
+        <div className="box3"></div>
+    </div>
+        </>
     )
 
     
--- src/Components/Register.js
+++ src/Components/Register.js
@@ -5,6 +5,25 @@ const Register = () => {
 
     return (
     <>
+    <div className="behind">
+        <ul>
+            <li><a href="/Appointments">Appointments</a></li>
+            <li><a href="/Patients">Patients</a></li>
+            <li><a href="/Doctors">Doctors</a></li>
+            <li><a href="/Reception">Reception</a></li>
+        </ul>
+        
+   
+    </div>
+    <div className="login-heading">
+    <p1>Sign Up</p1>
+        <div className="box4"><div className="email-login">Email</div></div>
+        <div className="box5"><div className="name-login">username</div></div>
+        <div className="box6"><div className="password-login">Password</div></div>
+        <div className="box7"><div className="confirm-login">Confirm Password</div></div>
+        <div className="box8"><div className="button-login"></div></div>
+        <div className="box9"></div>
+    </div>
     </>
     )
 
